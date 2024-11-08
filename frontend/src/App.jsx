e130d5b96187346fde77b45acde34fc498c47fde
Sameer Saharan <114926999+sameer-saharan@users.noreply.github.com>
2024-11-08 16:58:58 +0530

    Initial stage of frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
-import { TextGenerateEffect } from "../@/components/ui/text-generate-effect";
+import { TextGenerate, Lamp } from './components/index';
+import {Link} from 'react-router-dom';
 
 function App() {
-  const text = `Under Progress...`;
   return (
     <>
-      <div className='min-h-full flex items-center justify-center'>
-        <TextGenerateEffect words={text} className='text-6xl font-bold' />
+      <div className='h-screen p-5 flex flex-col items-center'>
+        <Lamp/>
+        <div className='flex flex-col items-center'>
+          <h2 className='font-bold text-2xl mb-5'>Reach out me at</h2>
+          <div className='flex flex-row gap-5'>
+            <Link to="mailto:sameerwebdev@outlook.com">
+              <img src="/email.png" alt="email" width="50px" 
+              className='hover:w-14 hover:shadow-lg hover:shadow-amber-300 rounded-full transition-all ease-linear' />
+            </Link>
+            
+            <Link to="https://x.com/sameerxdw">
+              <img src="/xtwitter.png" alt="twitter/x" width="50px" 
+              className='hover:w-14 hover:shadow-lg hover:shadow-amber-300 rounded-full transition-all ease-linear' />
+            </Link>
+
+            <Link to="https://github.com/sameer-saharan">
+              <img src="/github.png" alt="github" width="50px" 
+              className='hover:w-14 hover:shadow-lg hover:shadow-amber-300 rounded-full transition-all ease-linear' />
+            </Link>
+
+            <Link to="https://wa.me/917597940794">
+              <img src="/wa.png" alt="whatsapp" width="50px" 
+              className='hover:w-14 hover:shadow-lg hover:shadow-amber-300 rounded-3xl transition-all ease-linear' />
+            </Link>
+          </div>
+        </div>
       </div>
       
     </>
